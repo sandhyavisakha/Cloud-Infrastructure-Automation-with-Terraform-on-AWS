11e07d9f2bc162056cb8e19e4539e5e26f2257b7
sandhyavisakha <sandhyagriddaluru92@gmail.com>
2024-02-11 15:23:01 +0530

    added configuration for internet gateway and route tables

--- AWS-Project-with-Terraform/main.tf
+++ AWS-Project-with-Terraform/main.tf
@@ -15,3 +15,26 @@ resource "aws_subnet" "subnet-2" {
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
+
+resource "aws_internet_gateway" "igwtf" {
+  vpc_id = aws_vpc.myvpctf.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.myvpctf.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igwtf.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.subnet-2.id
+  route_table_id = aws_route_table.rt.id
+}
